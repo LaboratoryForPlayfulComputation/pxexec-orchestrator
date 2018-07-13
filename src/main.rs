f8593031ce9575ca0c8ca56695310e8d22d7130a
William Temple <William.Temple@colorado.edu>
2018-07-13 10:34:41 -0600

    Handled deserializing payload. Changed port to match editor configuration.
    Signed-off-by: William Temple <William.Temple@colorado.edu>

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,54 @@
 extern crate iron;
-extern crate rustc_serialize;
+
+extern crate serde;
+extern crate serde_json;
+
+extern crate unescape;
 
 use iron::prelude::*;
 use iron::status;
 
 use iron::method::Method;
 
-use rustc_serialize::json;
+use unescape::unescape;
 
+use std::collections::HashMap;
+use std::io::prelude::*;
+
+type RequestData = HashMap<String, String>;
+
+// This application is simple enough that I think we won't need router
+// middleware or similar. That option remains open, but for now I'll just return
+// 404 messages with a body that simply says "NOT FOUND" in case the message
+// is not a POST message or is not to simply "/save"
+//
+// In case the message is invalid, I'll send a generic 400 with the text
+// "YOUR FAULT" followed by a new line and any relevant error message, if one
+// exists.
+//
+/// Dispatch HTTP handler.
 fn handle_request(req: &mut Request) -> IronResult<Response> {
-    println!("{:?}", req);
+    println!("Message received: {:?}", req);
+    let mut data = String::new();
+    req.body.read_to_string(&mut data);
     match req.method {
         Method::Post => {
-            Ok(Response::with((status::Ok, "OK")))
+            let deserialized : Result<RequestData, serde_json::Error> = serde_json::from_str(&data);
+            match deserialized {
+                Ok(obj) => {
+                    if let Some(main_text) = obj.get("main.ts") {
+                        println!("{:?}", unescape(main_text).unwrap());
+                    }
+                    Ok(Response::with((status::Ok, "ACCEPTED")))
+                },
+                Err(msg) => Ok(Response::with((status::BadRequest, format!("YOUR FAULT\n{:?}", msg))))
+            }
         },
         _ => Ok(Response::with((status::NotFound, "NOT FOUND"))),
     }
 }
 
 fn main() {
-    println!("Starting server on port 3000.");
-    Iron::new(handle_request).http("localhost:3000").unwrap();
+    println!("Starting server on port 3074.");
+    Iron::new(handle_request).http("localhost:3074").unwrap();
 }
