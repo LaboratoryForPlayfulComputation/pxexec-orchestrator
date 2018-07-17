42b7be5f034149ded260aba1bade2606ff4a1186
William Temple <William.Temple@colorado.edu>
2018-07-17 17:28:15 -0600

    First working prototype of the full glue-code.
    Signed-off-by: William Temple <William.Temple@colorado.edu>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate iron;
+extern crate router;
 
 extern crate serde;
 extern crate serde_json;
@@ -9,23 +10,54 @@ mod tsc;
 
 use iron::prelude::*;
 use iron::status;
+use router::Router;
 
 use iron::method::Method;
 
 use std::collections::HashMap;
 use std::io::prelude::*;
+use std::process::Child;
+use std::sync::{Arc, Mutex};
 
 type RequestData = HashMap<String, String>;
 
-fn handle_request(body: RequestData) -> Result<(), String> {
+#[derive(Default)]
+struct PXExecState {
+    running_process: Arc<Mutex<Option<Child>>>,
+}
+
+impl PXExecState {
+    fn new() -> PXExecState {
+        PXExecState {
+            running_process: Arc::new(Mutex::new(None)),
+        }
+    }
+
+    fn replace_child(&self, c: Child) {
+        let mut cur_child = self.running_process.lock().unwrap();
+        *cur_child = Some(c);
+    }
+
+    fn take_child(&self) -> Option<Child> {
+        // This single line replaces running_process with None and returns
+        //   whatever it was before
+        self.running_process.lock().unwrap().take()
+    }
+}
+
+fn handle_request(body: RequestData, s: &PXExecState) -> Result<(), String> {
     if let Some(main_text) = body.get("main.ts") {
         let assembled = tsc::assemble_main(main_text);
         let mut bytes: &[u8] = assembled.as_bytes();
-        if let Err(msg) = tsc::compile_with_tempfile(&mut bytes) {
-            return Err(msg);
-        }
+        return match tsc::compile_with_tempfile(&mut bytes) {
+            Ok(path) => {
+                s.replace_child(tsc::execute(&path, s.take_child()));
+                Ok(())
+            }
+            Err(msg) => Err(msg),
+        };
     }
-    Ok(())
+    Err(String::from("No main.ts file was included in the bundle."))
 }
 
 // This application is simple enough that I think we won't need router
@@ -38,7 +70,7 @@ fn handle_request(body: RequestData) -> Result<(), String> {
 // exists.
 //
 /// Dispatch HTTP handler.
-fn validate_request(req: &mut Request) -> IronResult<Response> {
+fn validate_request(req: &mut Request, s: &PXExecState) -> IronResult<Response> {
     println!("Message received: {:?}", req);
     let mut data = String::new();
     req.body.read_to_string(&mut data).unwrap();
@@ -46,7 +78,7 @@ fn validate_request(req: &mut Request) -> IronResult<Response> {
         Method::Post => {
             let deserialized = serde_json::from_str(&data);
             match deserialized {
-                Ok(obj) => match handle_request(obj) {
+                Ok(obj) => match handle_request(obj, s) {
                     Ok(_) => Ok(Response::with((status::Ok, "ACCEPTED"))),
                     Err(msg) => Ok(Response::with((
                         status::InternalServerError,
@@ -65,5 +97,13 @@ fn validate_request(req: &mut Request) -> IronResult<Response> {
 
 fn main() {
     println!("Starting server on port 3074.");
-    Iron::new(validate_request).http("localhost:3074").unwrap();
+    let state = PXExecState::new();
+    let mut router = Router::new();
+
+    router.post(
+        "/save",
+        move |req: &mut Request| validate_request(req, &state),
+        "save",
+    );
+    Iron::new(router).http("localhost:3074").unwrap();
 }
--- src/tsc.rs
+++ src/tsc.rs
@@ -1,15 +1,15 @@
-use tempfile::tempfile;
-
-use std::fs::File;
+use std::env::temp_dir;
+use std::fs::{remove_file, File};
 use std::io::copy;
 use std::io::prelude::*;
-use std::process::Command;
+use std::path::{Path, PathBuf};
+use std::process::{Child, Command};
 
 const _PXEXEC_PREFIX: &'static str = r#"
 // BEGIN PXT_EXEC PREFIX
 
-import _pxexec from './core-exec';
-import loops from './loops';
+import _pxexec from '/usr/local/share/pxexec/core-exec';
+import loops from '/usr/local/share/pxexec/loops';
 
 _pxexec.init();
 
@@ -32,20 +32,48 @@ pub fn assemble_main(body: &str) -> String {
     String::from(_PXEXEC_PREFIX) + body + _PXEXEC_SUFFIX
 }
 
-pub fn compile_with_tempfile(contents: &mut Read) -> Result<(), String> {
-    let mut file = tempfile();
-    println!("{:?}", file);
-    match file {
-        Ok(ref mut file) => match copy(contents, file) {
-            Ok(_) => compile(file),
+pub fn compile_with_tempfile(contents: &mut Read) -> Result<PathBuf, String> {
+    let mut file_name = temp_dir();
+    let mut js_name = file_name.clone();
+    file_name.push("test.ts");
+    js_name.push("test.js");
+    let temp_file = File::create(file_name.clone());
+    match temp_file {
+        Ok(mut file) => match copy(contents, &mut file) {
+            Ok(_) => {
+                compile(&file_name)?;
+                remove_file(file_name).unwrap();
+                println!("Successfully compiled!");
+                Ok(js_name)
+            }
             Err(msg) => Err(msg.to_string()),
         },
         Err(msg) => Err(msg.to_string()),
     }
 }
 
-pub fn compile(file: &mut File) -> Result<(), String> {
+pub fn compile(path: &Path) -> Result<(), String> {
     let compiler = Command::new("tsc")
-        .arg(file.path)
+        .arg("--target")
+        .arg("ES5")
+        .arg("--module")
+        .arg("commonjs")
+        .arg("--moduleResolution")
+        .arg("node")
+        .arg(path)
+        .output()
+        .expect("failed to execute tsc");
+    println!("{}", String::from_utf8(compiler.stdout).unwrap());
     Ok(())
+}
+
+pub fn execute(path: &Path, prev: Option<Child>) -> Child {
+    match prev {
+        Some(mut child) => {
+            child.kill().unwrap();
+        }
+        None => {}
+    }
+
+    Command::new("node").arg(path).spawn().unwrap()
 }
